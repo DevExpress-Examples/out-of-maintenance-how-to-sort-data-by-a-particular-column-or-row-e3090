310cf0c02449ac5f5591b6ba1bc94adbff683211
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXPivotGrid_SortBySummary/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using DevExpress.Xpf.PivotGrid;
+
+namespace DXPivotGrid_SortBySummary {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            pivotGridControl1.DataSource = 
+                (new nwindDataSetTableAdapters.SalesPersonTableAdapter()).GetData();
+        }
+        private void pivotGridControl1_Loaded(object sender, RoutedEventArgs e) {
+
+            // Locks the pivot grid from updating while the Sort by Summary
+            // settings are being customized.
+            pivotGridControl1.BeginUpdate();
+            try {
+
+                // Specifies a data field whose summary values should be used to sort values
+                // of the Product Name field.
+                fieldProductName.SortByField = fieldUnitPrice;
+
+                // Specifies a column by which the Product Name field values should be sorted.
+                fieldProductName.SortByConditions.Add(new SortByCondition(fieldYear, "1994"));
+                fieldProductName.SortByConditions.Add(new SortByCondition(fieldMonth, "9"));
+            }
+            finally {
+
+                // Unlocks the pivot grid and applies changes.
+                pivotGridControl1.EndUpdate();
+            }
+        }
+    }
+}
---
+++ VB/DXPivotGrid_SortBySummary/MainWindow.xaml.vb
@@ -0,0 +1,34 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports DevExpress.Xpf.PivotGrid
+
+Namespace DXPivotGrid_SortBySummary
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			pivotGridControl1.DataSource = _
+				(New nwindDataSetTableAdapters.SalesPersonTableAdapter()).GetData()
+		End Sub
+		Private Sub pivotGridControl1_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+
+			' Locks the pivot grid from updating while the Sort by Summary
+			' settings are being customized.
+			pivotGridControl1.BeginUpdate()
+			Try
+
+				' Specifies a data field whose summary values should be used to sort values
+				' of the Product Name field.
+				fieldProductName.SortByField = fieldUnitPrice
+
+				' Specifies a column by which the Product Name field values should be sorted.
+				fieldProductName.SortByConditions.Add(New SortByCondition(fieldYear, "1994"))
+				fieldProductName.SortByConditions.Add(New SortByCondition(fieldMonth, "9"))
+			Finally
+
+				' Unlocks the pivot grid and applies changes.
+				pivotGridControl1.EndUpdate()
+			End Try
+		End Sub
+	End Class
+End Namespace
